450e01abb415e9e7a6ca2dc900b48394b247dc66
Fabian Allendorf <fabian.allendorf@gmail.com>
2022-01-15 00:35:28 +0100

    replace favicon and update title

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,12 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # abort on errors
 set -e
 
-# build
 npm run build
 
-# navigate into the build output directory
 cd dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-git init
 git add -A
 git commit -m 'deploy'
 
